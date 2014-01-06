b4b62d06de7865cb13626038438e3e9e5b640f4f
Figglewatts <figglewatts@hotmail.co.uk>
2014-01-06 18:07:51 +0000

    Added Node class for any nodes to extend

---
+++ src/figglewatts/SEGment/Node.java
@@ -0,0 +1,9 @@
+package figglewatts.SEGment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+	public String[] dependencies; // put the names of any components that this depends on here (e.g. "Position", and "Display")
+	public List<Component> components = new ArrayList<Component>();
+}
