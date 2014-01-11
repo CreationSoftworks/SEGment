2643caab4dd67c84264f17f560735b171e00117b
Figglewatts <figglewatts@hotmail.co.uk>
2014-01-11 16:53:47 +0000

    Removed DependencyList (deprecated)

--- src/figglewatts/SEGment/DependencyList.java
+++
@@ -1,7 +0,0 @@
-package figglewatts.SEGment;
-
-import java.util.ArrayList;
-
-public class DependencyList {
-	public ArrayList<ArrayList<String>> dependencies = new ArrayList<ArrayList<String>>();
-}
